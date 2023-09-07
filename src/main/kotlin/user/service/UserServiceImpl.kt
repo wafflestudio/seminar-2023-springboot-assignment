a0014fe7193a03040484e48b7c3827431d61b067
andhl <andhlwhffu@gmail.com>
2023-09-07 20:43:37 +0900

    signUp-UserServiceImpl

--- src/main/kotlin/user/service/UserServiceImpl.kt
+++ src/main/kotlin/user/service/UserServiceImpl.kt
@@ -1,15 +1,27 @@
 package com.wafflestudio.seminar.spring2023.user.service
 
+import com.wafflestudio.seminar.spring2023.user.repository.UserEntity
 import com.wafflestudio.seminar.spring2023.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
     private val userRepository: UserRepository,
+    private var id: Long = 0
 ) : UserService {
 
     override fun signUp(username: String, password: String, image: String): User {
-        TODO("Not yet implemented")
+        if (username.length < 4) {
+            throw SignUpBadUsernameException()
+        } else if (password.length < 4) {
+            throw SignUpBadPasswordException()
+        } else if (userRepository.findByUsername(username) != null) {
+            throw SignUpUsernameConflictException()
+        } else {
+            val user = UserEntity(id++, username, password, image)
+            userRepository.save(user)
+            return User(username, image)
+        }
     }
 
     override fun signIn(username: String, password: String): User {
