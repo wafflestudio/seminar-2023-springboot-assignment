e7538631702e30ccdfe33084f79fed12e9dcac04
andhl <andhlwhffu@gmail.com>
2023-09-07 21:18:38 +0900

    signup-UserController

--- src/main/kotlin/user/controller/UserController.kt
+++ src/main/kotlin/user/controller/UserController.kt
@@ -1,12 +1,18 @@
 package com.wafflestudio.seminar.spring2023.user.controller
 
+import com.wafflestudio.seminar.spring2023.user.repository.UserEntity
+import com.wafflestudio.seminar.spring2023.user.service.SignUpBadPasswordException
+import com.wafflestudio.seminar.spring2023.user.service.SignUpBadUsernameException
+import com.wafflestudio.seminar.spring2023.user.service.SignUpUsernameConflictException
 import com.wafflestudio.seminar.spring2023.user.service.UserService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import java.lang.Exception
 
 @RestController
 class UserController(
@@ -17,7 +23,20 @@ class UserController(
     fun signup(
         @RequestBody request: SignUpRequest,
     ): ResponseEntity<Unit> {
-        TODO()
+        return try {
+            userService.signUp(request.username, request.password, request.image)
+            ResponseEntity.status(200).build()
+        } catch (ex: Exception) {
+            when(ex) {
+                is SignUpBadUsernameException, is SignUpBadPasswordException -> {
+                    ResponseEntity.status(400).build()
+                }
+                is SignUpUsernameConflictException -> {
+                    ResponseEntity.status(409).build()
+                }
+                else -> throw ex
+            }
+        }
     }
 
     @PostMapping("/api/v1/signin")
