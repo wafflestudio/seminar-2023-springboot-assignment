0285ba1382a3fac9aa04dcf1f323cc870735a1b8
minkyu97 <whizkyu@snu.ac.kr>
2023-09-08 13:55:40 +0900

    solve: UserIntegrationTest.kt

--- src/main/kotlin/user/controller/UserController.kt
+++ src/main/kotlin/user/controller/UserController.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.seminar.spring2023.user.controller
 
+import com.wafflestudio.seminar.spring2023.user.service.*
 import com.wafflestudio.seminar.spring2023.user.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -17,21 +18,58 @@ class UserController(
     fun signup(
         @RequestBody request: SignUpRequest,
     ): ResponseEntity<Unit> {
-        TODO()
+        return try {
+            userService.signUp(request.username, request.password, request.image)
+            ResponseEntity.ok().build()
+        } catch (e: UserException) {
+            val statusCode = handleUserException(e)
+            ResponseEntity.status(statusCode).build()
+        }
     }
 
     @PostMapping("/api/v1/signin")
     fun signIn(
         @RequestBody request: SignInRequest,
     ): ResponseEntity<SignInResponse> {
-        TODO()
+        return try {
+            val user = userService.signIn(request.username, request.password)
+            ResponseEntity.ok(
+                SignInResponse(
+                    accessToken = user.getAccessToken(),
+                )
+            )
+        } catch (e: UserException) {
+            val statusCode = handleUserException(e)
+            ResponseEntity.status(statusCode).build()
+        }
     }
 
     @GetMapping("/api/v1/users/me")
     fun me(
         @RequestHeader(name = "Authorization", required = false) authorizationHeader: String?,
     ): ResponseEntity<UserMeResponse> {
-        TODO()
+        return try {
+            val accessToken = authorizationHeader?.removePrefix("Bearer ") ?: throw AuthenticateException()
+            val user = userService.authenticate(accessToken)
+            ResponseEntity.ok(
+                UserMeResponse(
+                    username = user.username,
+                    image = user.image,
+                )
+            )
+        } catch (e: UserException) {
+            val statusCode = handleUserException(e)
+            ResponseEntity.status(statusCode).build()
+        }
+    }
+
+    fun handleUserException(ex: UserException): Int = when (ex) {
+        is SignUpBadUsernameException -> 400
+        is SignUpBadPasswordException -> 400
+        is SignUpUsernameConflictException -> 409
+        is SignInUserNotFoundException -> 404
+        is SignInInvalidPasswordException -> 404
+        is AuthenticateException -> 401
     }
 }
 
