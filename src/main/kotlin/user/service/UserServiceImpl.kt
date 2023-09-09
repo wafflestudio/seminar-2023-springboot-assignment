c146c34024036695ebd95492595aa95ca9ce5d3d
isuk <ihyungsuk@snu.ac.kr>
2023-09-09 18:37:03 +0900

    FEAT : UserServiceImpl 작업

--- src/main/kotlin/user/service/UserServiceImpl.kt
+++ src/main/kotlin/user/service/UserServiceImpl.kt
@@ -1,22 +1,40 @@
 package com.wafflestudio.seminar.spring2023.user.service
 
+import com.wafflestudio.seminar.spring2023.user.repository.UserEntity
 import com.wafflestudio.seminar.spring2023.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
-    private val userRepository: UserRepository,
+  private val userRepository: UserRepository,
 ) : UserService {
 
-    override fun signUp(username: String, password: String, image: String): User {
-        TODO("Not yet implemented")
-    }
+  override fun signUp(username: String, password: String, image: String): User {
+    if (userRepository.findByUsername(username) != null) throw SignUpUsernameConflictException()
+    if (username.length < 4) throw SignUpBadUsernameException()
+    if (password.length < 4) throw SignUpBadPasswordException()
 
-    override fun signIn(username: String, password: String): User {
-        TODO("Not yet implemented")
-    }
+    val newUserEntity = UserEntity(
+      id = userRepository.count() + 1,
+      username = username,
+      password = password,
+      image = image
+    )
+    userRepository.save(newUserEntity)
+    return User(username = username, image = image)
+  }
 
-    override fun authenticate(accessToken: String): User {
-        TODO("Not yet implemented")
-    }
+  override fun signIn(username: String, password: String): User {
+    val userEntity = userRepository.findByUsername(username)
+      ?: throw SignInUserNotFoundException()
+    if (userEntity.password != password) throw SignInInvalidPasswordException()
+    return User(username, userEntity.image)
+  }
+
+  override fun authenticate(accessToken: String): User {
+    return userRepository.findAll()
+      .map { User(it.username, it.image) }
+      .find { it.getAccessToken() == accessToken }
+      ?: throw AuthenticateException()
+  }
 }
