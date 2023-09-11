6dcb5a43d3f5dd4abd1a86cf25876882f7633001
eastshine2741 <eastshine@snu.ac.kr>
2023-09-11 21:26:34 +0900

    typo

--- src/main/kotlin/user/AuthUser.kt
+++ src/main/kotlin/user/AuthUser.kt
@@ -1,10 +1,5 @@
 package com.wafflestudio.seminar.spring2023.user
 
-import java.lang.annotation.ElementType
-import java.lang.annotation.RetentionPolicy
-
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class AuthUser {
-
-}
+annotation class AuthUser
