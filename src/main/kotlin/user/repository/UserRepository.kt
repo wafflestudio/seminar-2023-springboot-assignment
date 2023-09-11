e51e10a8331ee1e93b832975d76840ae92092513
eastshine2741 <eastshine@snu.ac.kr>
2023-09-11 18:07:57 +0900

    UserServiceImpl 구현

--- src/main/kotlin/user/repository/UserRepository.kt
+++ src/main/kotlin/user/repository/UserRepository.kt
@@ -2,4 +2,8 @@ package com.wafflestudio.seminar.spring2023.user.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<UserEntity, Long>
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun findByUsername(username: String): UserEntity?
+
+    fun existsByUsername(username: String): Boolean
+}
