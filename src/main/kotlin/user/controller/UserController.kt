6fc8d9c678f7d8dcb5a8b668948bf3d3fe76d453
navgod <buzz2604@gmail.com>
2023-09-15 13:47:35 +0900

    Test 완료

--- src/main/kotlin/user/controller/UserController.kt
+++ src/main/kotlin/user/controller/UserController.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.seminar.spring2023.user.controller
 
-import com.wafflestudio.seminar.spring2023.user.service.UserService
+import com.wafflestudio.seminar.spring2023.user.service.*
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,21 +18,61 @@ class UserController(
     fun signup(
         @RequestBody request: SignUpRequest,
     ): ResponseEntity<Unit> {
-        TODO()
+        try{
+            // request에서 username, password,image를 입력 받아서 userService를 생성
+            userService.signUp(request.username,request.password,request.image)
+            // 정상적이라면 200응답
+            return ResponseEntity.ok().build()
+        }
+        // 이름에 문제가 있을 경우 400 응답
+        catch (e: SignUpBadUsernameException){
+            return ResponseEntity.badRequest().build()
+        }
+        // 비밀번호에 문제가 있을 경우 400 응답
+        catch (e: SignUpBadPasswordException){
+            return ResponseEntity.badRequest().build()
+        }
+        // 이미 존재하는 유저 이름으로 가입할 경우 409 응답
+        catch (e: SignUpUsernameConflictException){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build()
+        }
     }
 
     @PostMapping("/api/v1/signin")
     fun signIn(
         @RequestBody request: SignInRequest,
     ): ResponseEntity<SignInResponse> {
-        TODO()
+        try{
+            // 입력한 이름과 비밀번호로 접속 시도
+            val user = userService.signIn(request.username,request.password)
+            // 정상적이라면 200 응답
+            return ResponseEntity.ok(SignInResponse(accessToken = user.getAccessToken()))
+        }
+        catch (e: SignInUserNotFoundException){
+            // 해당 유저가 존재하지 않을 때 404 응답
+            return ResponseEntity.notFound().build()
+        }
+        catch (e: SignInInvalidPasswordException){
+            // 비밀번호가 틀렸을 때 404 응답
+            return ResponseEntity.notFound().build()
+        }
     }
 
     @GetMapping("/api/v1/users/me")
     fun me(
         @RequestHeader(name = "Authorization", required = false) authorizationHeader: String?,
     ): ResponseEntity<UserMeResponse> {
-        TODO()
+        //헤더에서 접두사 Bearer 제거 후 null인지 확인하고 null 이면 AuthenticateException를 발생
+        val token = authorizationHeader?.removePrefix("Bearer ") ?: throw AuthenticateException()
+        try{
+            val user = userService.authenticate(token)
+            // 정상적으로 인증이 되었다면 200 응답
+            return ResponseEntity.ok(UserMeResponse(username = user.username,image = user.image))
+        }
+        catch (e : AuthenticateException){
+            // 인증에 오류가 있다면 401 응답
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
+        }
     }
 }
 
--- src/main/kotlin/user/service/UserServiceImpl.kt
+++ src/main/kotlin/user/service/UserServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.seminar.spring2023.user.service
 
+import com.wafflestudio.seminar.spring2023.user.repository.UserEntity
 import com.wafflestudio.seminar.spring2023.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -9,14 +10,42 @@ class UserServiceImpl(
 ) : UserService {
 
     override fun signUp(username: String, password: String, image: String): User {
-        TODO("Not yet implemented")
+        //유저의 이름이 4자 미만이면 SignUpBadUsernameException() 발생
+        if(username.length < 4) throw SignUpBadUsernameException()
+        //유저의 비밀번호가 4자 미만미면 SignUpBadPasswordException() 발생
+        if(password.length < 4) throw SignUpBadPasswordException()
+
+        // 가입하는 유저가 중복된 이름인지 확인
+        userRepository.findByUsername(username)?.let {
+            throw SignUpUsernameConflictException()
+        }
+
+        //중복이 아니라면 새로운 유저 생성
+        val userEntity = UserEntity(username= username,password = password,image = image)
+        val savedUser = userRepository.save(userEntity)
+
+        return User( username= savedUser.username, image = savedUser.image)
     }
 
     override fun signIn(username: String, password: String): User {
-        TODO("Not yet implemented")
+        // findByUsername을 통해 일치하는 회원이 있는지 확인
+        val userEntity = userRepository.findByUsername(username)
+            ?: throw SignInUserNotFoundException()
+        // 비밀번호가 맞는지 확인
+        if(userEntity.password != password){
+            throw SignInInvalidPasswordException()
+        }
+        // 모두 일치한다면 유저 반환
+        return User(username= userEntity.username, image = userEntity.image)
     }
 
     override fun authenticate(accessToken: String): User {
-        TODO("Not yet implemented")
+        // 토큰을 다시 유저 이름으로 변환
+        val username = accessToken.reversed()
+        // 유저 네임을 통해 찾고 엘비스 연산자를 통해 null 확인
+        val userEntity = userRepository.findByUsername(username)
+            ?: throw AuthenticateException()
+        // 다 통과한다면 유저 반환
+        return User(username = userEntity.username, image = userEntity.image)
     }
 }
