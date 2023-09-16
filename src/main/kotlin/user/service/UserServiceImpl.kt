83c6a0879b8329089bbd2343cfbac048f2705ba1
yee12300 <128353782+yee12300@users.noreply.github.com>
2023-09-16 18:46:36 +0900

    2번 과제 수행
    UserServiceTest 통과

--- src/main/kotlin/user/service/UserServiceImpl.kt
+++ src/main/kotlin/user/service/UserServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.wafflestudio.seminar.spring2023.user.service
 
+import com.wafflestudio.seminar.spring2023.user.repository.UserEntity
 import com.wafflestudio.seminar.spring2023.user.repository.UserRepository
 import org.springframework.stereotype.Service
 
@@ -9,14 +10,23 @@ class UserServiceImpl(
 ) : UserService {
 
     override fun signUp(username: String, password: String, image: String): User {
-        TODO("Not yet implemented")
+        when {
+            username.length < 4 -> throw SignUpBadUsernameException()
+            password.length < 4 -> throw SignUpBadPasswordException()
+            userRepository.findByUsername(username) != null -> throw SignUpUsernameConflictException()
+        }
+        userRepository.save(UserEntity(username = username, password = password, image = image))
+        return User(username = username, image = image)
     }
 
     override fun signIn(username: String, password: String): User {
-        TODO("Not yet implemented")
+        val user = userRepository.findByUsername(username) ?: throw SignInUserNotFoundException()
+        if(password != user.password) throw SignInInvalidPasswordException()
+        return User(username = username, image = user.image)
     }
 
     override fun authenticate(accessToken: String): User {
-        TODO("Not yet implemented")
+        val user = userRepository.findByUsername(accessToken.reversed()) ?: throw AuthenticateException()
+        return User(username = user.username, image = user.image)
     }
 }
