e1b3ba2c02f41a98f25cd0cfad3a9183186e25dd
jj1kim <jeewonbob@gmail.com>
2023-09-18 04:06:22 +0900

    사전 과제 및 세미나 0 수강 내용

--- src/main/kotlin/SeminarApplication.kt
+++ src/main/kotlin/SeminarApplication.kt
@@ -2,6 +2,10 @@ package com.wafflestudio.seminar.spring2023
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SeminarApplication
@@ -10,4 +14,20 @@ fun main() {
     runApplication<SeminarApplication>()
 }
 
+@RestController //REST API 서버
+class DemoController{
 
+    @GetMapping("/users/{userId}") //GET METHOD, users/1, users/2, ...
+    fun getDemoUser(
+        @PathVariable userId: Long,
+    ): ResponseEntity<DemoUser> {//HTTP, DemoUser
+        val user = DemoUser(id = userId, name = "user-$userId")
+
+        return ResponseEntity.status(200).body(user)
+    }
+}
+
+class DemoUser(
+    val id : Long,
+    val name : String,
+)
--- src/test/kotlin/kotlin/KotlinTest.kt
+++ src/test/kotlin/kotlin/KotlinTest.kt
@@ -20,7 +20,10 @@ class KotlinTest {
     @Test
     fun `모바일 타입의 SeminarBrief 목록을, 시작날짜를 기준으로 오름차순 정렬`() {
         // FIXME: seminaDetails에 filter, sortedBy, map을 사용하여 구현 (KotlinCheetSheet.kt 참고)
-        val answer: List<SeminarBrief> = listOf()
+        val answer: List<SeminarBrief> = seminarDetails
+                .filter{it.type == Type.MOBILE }
+                .map { SeminarBrief(name=it.name, startAt = it.startAt) }
+                .sortedBy{it.startAt}
 
         assertThat(answer).isEqualTo(answers2)
     }
@@ -31,6 +34,14 @@ class KotlinTest {
         var mobileCnt = 0
         var webCnt = 0
         var serverCnt = 0
+
+        seminarDetails.forEach{
+            when (it.type){
+                Type.MOBILE -> mobileCnt+=1
+                Type.WEB -> webCnt+=1
+                Type.SERVER -> serverCnt+=1
+            }
+        }
 
         assertThat(mobileCnt).isEqualTo(2)
         assertThat(webCnt).isEqualTo(1)
