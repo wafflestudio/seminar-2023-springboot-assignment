e2a2945beb3e68ececfdb8075fa2c2ec32374613
jj1kim <jeewonbob@gmail.com>
2023-09-18 21:47:28 +0900

    추가 과제(UserControllerV2 구현을 위한 ExceptionHandler 작업과 UserArgumentResolver.kt에 HandlerMethodArgumentResolver 구현)

--- src/main/kotlin/user/UserArgumentResolver.kt
+++ src/main/kotlin/user/UserArgumentResolver.kt
@@ -10,30 +10,41 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-// TODO: 추가과제
+/* TODO: 추가과제
+HandlerMethodArgumentResolver는 메소드 파라미터들을 인자값들에 주입해주는 인터페이스이다
+HandlerMethodArgumentResolver를 유형으로 가지고, 내부의 로직처리를 위해 UserService를 상속하는 커스텀 HandlerMethodArgumentResolver를 구현해주면 된다(그게 바로 UserArgumentResolver)
+내부에는 어떤 파라미터들에 대해서 작업을 수행할건지(지원할 건지) 여부를 결정하는 supportsParameter와, 실제로 그 파라미터들을 가지고 어떤 로직으로 처리를 해줄지 결정하는 resolveArgument를 정의해줘야 한다. */
+
+
 class UserArgumentResolver(
-    private val userService: UserService,
+    private val userService: UserService, //resolveArgument에서의 로직 처리를 위해 상속 받는다.
 ) : HandlerMethodArgumentResolver {
 
-    override fun supportsParameter(parameter: MethodParameter): Boolean {
-        TODO()
-    }
+    override fun supportsParameter(parameter: MethodParameter): Boolean { //처리 대상이 되는 파라미터를 지정하기 때문에 boolean 형이다.
+        return parameter.parameterType == User::class.java  /*User에 정의되어 있는 모든 파라미터들을 대상으로 설정하는 코드이다.(::를 이용해서 각 파라미터가 정의되는 class들을 한번에 포함시킨다)*/
+    } //비록 지금은 User 내의 image에 대해서만 사용하긴 하겠지만 일단 다 지정해두자
 
-    override fun resolveArgument(
+    override fun resolveArgument(//실제 해당 파라미터들로 처리해야할 로직에 대해서 정의한다. 우리는 응답 토큰의 처리에 대해서 간결하게 만들고 싶은 거니까 그것만 정의하면 된다.
         parameter: MethodParameter,
         mavContainer: ModelAndViewContainer?,
         webRequest: NativeWebRequest,
-        binderFactory: WebDataBinderFactory?,
+        binderFactory: WebDataBinderFactory?, //여기에 있는 4개 파라미터는 원래 이렇게 정의해야하는 듯 하다(이유는 모르겠음)
     ): User {
-        TODO()
+        val accessToken = webRequest.getHeader("Authorization")?.split(" ")?.get(1) ?: ""
+        /*토큰을 만드는 과정을 미리 처리하기 위해 위와 같은 코드를 작성했다.
+        webRequest로 url 형식으로 들어오는 요청 헤더에서 키가 "Authorization"인 요청의 밸류만 빼낸뒤, 띄어쓰기를 기준으로 갈라서 두 번째 요소인 진짜 토큰을 빼내는 코드이다.
+        만약 그러한 요청이 없거나 쪼갤 것이 없거나 한다면 그냥 빈 문자열을 대입하게 된다.
+        */
+
+        return userService.authenticate(accessToken) //이렇게 만든 토큰을 상속받아둔 userService의 authenticate로 리턴하면 토큰에 관한 처리는 끝이다. 나머지는 Controller에서 응답 처리를 해주면 된다
     }
 }
 
-@Configuration
+@Configuration //구현한 HandlerMethodArgumentResolver를 사용하려면 addArgumentResolver에 등록해줘야 한다. 아래가 그 과정이 되겠다.
 class WebConfig(
     private val userService: UserService,
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
-        resolvers.add(UserArgumentResolver(userService))
+        resolvers.add(UserArgumentResolver(userService)) //userService에 대한 UserArgumentResolver의 사용을 등록한다.
     }
 }
--- src/main/kotlin/user/controller/UserControllerV2.kt
+++ src/main/kotlin/user/controller/UserControllerV2.kt
@@ -1,8 +1,8 @@
 package com.wafflestudio.seminar.spring2023.user.controller
 
-import com.wafflestudio.seminar.spring2023.user.service.User
-import com.wafflestudio.seminar.spring2023.user.service.UserException
-import com.wafflestudio.seminar.spring2023.user.service.UserService
+import com.wafflestudio.seminar.spring2023.user.service.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode  //응답 상태를 내려줄 HttpStatus가 정의가 안되있길래 넣었다
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,7 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 
-// TODO: 추가 과제 ExceptionHandler와 HandlerMethodArgumentResolver 적용
+/* TODO: 추가 과제 ExceptionHandler와 HandlerMethodArgumentResolver 적용
+원래 UserController 구현시에는 각각의 동작마다 예외를 따로 처리해야해서 굉장히 복잡했음
+-> ExceptionHandler라는 annotation을 통해 예외처리 객체를 따로 생성함으로써 예외를 한방에 처리할 수 있다
+원래 UserController 구현시에는 인증토큰 확인 및 유저 응답처리시 직접 요청 Header에서 authorization 부분의 토큰을 떼와서 처리해야 하는 불편함이 있었음
+me 함수의 파라미터를 HandlerMethodArgumentResolver를 통해서 따로 간단히 처리하고자 함
+HandlerMethodArgumentResolver를 쓰면 User의 파라미터들을 간단하게 처리할 수 있다
+-> UserArgumentResolver에서 사용해서 불러오므로 거기서 자세히 설명한다*/
 @RestController
 class UserControllerV2(
     private val userService: UserService,
@@ -21,23 +27,29 @@ class UserControllerV2(
     fun signup(
         @RequestBody request: SignUpRequest,
     ) {
-        TODO()
+        userService.signUp(request.username,request.password,request.image) //예외처리가 여기서는 필요 없으므로 signup만 시켜주면 된다
     }
 
     @PostMapping("/api/v2/signin")
     fun signIn(
         @RequestBody request: SignInRequest,
     ): SignInResponse {
-        TODO()
+        val user = userService.signIn(request.username,request.password)
+        return SignInResponse(user.getAccessToken()) //예외처리가 필요 없으므로 요청사항에 맞는 유저를 찾아서 인증 토큰을 응답해주면 된다.
     }
 
     @GetMapping("/api/v2/users/me")
     fun me(user: User): UserMeResponse {
-        TODO()
+        return UserMeResponse(user.username,user.image) //UserArgumentResolver에서 파라미터와 로직처리를 다 해주므로 그냥 들어온 유저네임과 인증토큰을 가지고 인증여부 응답만 내려주면 된다
     }
 
-    @ExceptionHandler
+    @ExceptionHandler //모든 종류의 예외처리를 따로 빼서 한방에 진행할 수 있는 annotation이다
     fun handleException(e: UserException): ResponseEntity<Unit> {
-        TODO()
+        return when(e){
+            is SignUpBadUsernameException, is SignUpBadPasswordException -> ResponseEntity(HttpStatus.BAD_REQUEST) //400 에러 구현
+            is SignUpUsernameConflictException -> ResponseEntity(HttpStatus.CONFLICT) //409 에러 구현
+            is SignInUserNotFoundException, is SignInInvalidPasswordException -> ResponseEntity(HttpStatus.NOT_FOUND) //404 에러 구현
+            is AuthenticateException -> ResponseEntity(HttpStatus.UNAUTHORIZED) //401 에러 구현
+        }
     }
 }
