88f495ff21893ab0855911b63daf821ae8f18733
Eugene Sohn <qeugene2328@gmail.com>
2023-09-18 22:20:18 +0900

    Update UserServiceImpl.kt

--- src/main/kotlin/user/service/UserServiceImpl.kt
+++ src/main/kotlin/user/service/UserServiceImpl.kt
@@ -6,52 +6,47 @@ import org.springframework.stereotype.Service
 
 @Service
 class UserServiceImpl(
-    private val userRepository: UserRepository,
+        private val userRepository: UserRepository,
 ) : UserService {
-
     override fun signUp(username: String, password: String, image: String): User {
         if (username.length < 4) {
             throw SignUpBadUsernameException()
-        }
-
-        if (password.length < 4) {
+        } else if (password.length < 4) {
             throw SignUpBadPasswordException()
+        } else {
+            val userList = userRepository.findAll()
+            for (users in userList) {
+                if (users.username.equals(username)) {
+                    throw SignUpUsernameConflictException()
+                }
+            }
+            val userEntity = UserEntity(username = username, password = password, image = image)
+            userRepository.save(userEntity)
+            return User(userEntity.username, userEntity.image)
         }
-
-        if (userRepository.findByUsername(username) != null) {
-            throw SignUpUsernameConflictException()
-        }
-
-        val entity = userRepository.save(
-            UserEntity(
-                username = username,
-                password = password,
-                image = image
-            )
-        )
-
-        return User(entity)
     }
 
     override fun signIn(username: String, password: String): User {
-        val entity = userRepository.findByUsername(username) ?: throw SignInUserNotFoundException()
-
-        if (entity.password != password) {
-            throw SignInInvalidPasswordException()
+        val userList = userRepository.findAll()
+        for (users in userList) {
+            if (users.username.equals(username)) {
+                if (users.password.equals(password)) {
+                    return User(username, password)
+                } else {
+                    throw SignInInvalidPasswordException()
+                }
+            }
         }
-
-        return User(entity)
+        throw SignInUserNotFoundException()
     }
 
     override fun authenticate(accessToken: String): User {
-        val entity = userRepository.findByUsername(accessToken.reversed()) ?: throw AuthenticateException()
-
-        return User(entity)
+        val userList = userRepository.findAll()
+        for (users in userList) {
+            if (users.username.equals(accessToken.reversed())) {
+                return User(users.username, users.image)
+            }
+        }
+        throw AuthenticateException()
     }
 }
-
-fun User(entity: UserEntity) = User(
-    id = entity.id,
-    username = entity.username,
-    image = entity.image,
-)
