6e52ccf428dd0d10d208f97fbfe392caadbe5d6b
lhw414 <dlgusdn0414@snu.ac.kr>
2023-09-27 13:38:50 +0900

    Feat&Test : song integration test code 작성 및 테스트 완료

--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -1,13 +1,39 @@
 package com.wafflestudio.seminar.spring2023.song
 
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
 ) {
+    @Test
+    fun `특정 키워드로 곡을 조회할 수 있다`() {
+        mvc.perform(
+            get("/api/v1/songs")
+                .param("keyword", "lucky")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().`is`(200))
+            .andExpect(jsonPath("$.songs").exists())
+    }
+
+    @Test
+    fun `특정 키워드로 앨범을 조회할 수 있다`() {
+        mvc.perform(
+            get("/api/v1/albums")
+                .param("keyword", "lucky")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.albums").exists())
+    }
 }
