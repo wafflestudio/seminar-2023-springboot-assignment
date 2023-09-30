e59dc5084eacbd8ad16693d9b31e199288b1698c
박수민 <parksumin@bagsumin-ui-MacBookAir.local>
2023-09-30 23:55:54 +0900

    Song Intergration Test 구현

--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -15,6 +15,38 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
 ) {
+    @Test
+    fun `있는 노래 제목을 검색했을 때 존재해야 함`() {
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/songs")
+                .param("keyword", "boy")
+                .contentType(MediaType.APPLICATION_JSON)
+
+        )
+            .andExpect(status().`is`(200))
+            .andExpect(jsonPath("$.songs").exists())
+
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/songs")
+                .param("keyword", "seven")
+                .contentType(MediaType.APPLICATION_JSON)
+
+        )
+            .andExpect(status().`is`(200))
+            .andExpect(jsonPath("$.songs").exists())
+    }
+
+    @Test
+    fun `없는 노래 제목을 검색했을 때 존재하지 않아야 함`() {
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/songs")
+                .param("keyword", "wow")
+                .contentType(MediaType.APPLICATION_JSON)
+
+        )
+            .andExpect(status().isNotFound())
+
+    }
 
 
 }
