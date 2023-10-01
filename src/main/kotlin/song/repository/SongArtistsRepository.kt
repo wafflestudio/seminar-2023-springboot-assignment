b73d96ec926d21c00f659b1000b8c59a9fea6ee2
Chanyeong <dlacksdud2@snu.ac.kr>
2023-10-01 20:33:53 +0900

    Week1 : 기본 과제 완료

--- src/main/kotlin/song/repository/SongArtistsRepository.kt
+++ src/main/kotlin/song/repository/SongArtistsRepository.kt
@@ -10,6 +10,6 @@ interface SongArtistsRepository : JpaRepository<SongArtistsEntity, Long> {
     @Query("SELECT a FROM song_artists a JOIN FETCH a.song LEFT JOIN FETCH a.artist WHERE a.song.id = :song_id")
     fun findBySongId(song_id : Long) : List<SongArtistsEntity>
 
-    @Query("SELECT a FROM song_artists a JOIN FETCH a.song LEFT JOIN FETCH a.artist LEFT JOIN FETCH a.song.playlistSongs JOIN FETCH a.song.album JOIN FETCH a.song.album.artist  JOIN FETCH a.artist.songArists WHERE a.song.title like %:keyword%")
+    @Query("SELECT a FROM song_artists a LEFT JOIN FETCH a.song JOIN FETCH a.artist JOIN FETCH a.song.album JOIN FETCH a.song.album.artist  JOIN FETCH a.artist.songArists WHERE a.song.title like %:keyword%")
     fun findBySongTitleContaining(keyword: String) : List<SongArtistsEntity>
 }
--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -17,9 +17,32 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
-) {
+    private val mapper: ObjectMapper,
+    ) {
     @Test
-    fun `곡 검색 결과가 없는 경우`(){
-
+    fun `곡 이름으로 검색 결과 확인`(){
+        mvc.perform(
+                get("/api/v1/songs").param("keyword","Don't")
+        )
+                .andExpect(status().`is`(200))
+                .andExpect(jsonPath("songs.[1].title").value("Don't Get Me Wrong"))
+        mvc.perform(
+                get("/api/v1/songs").param("keyword","????????")
+        )
+                .andExpect(status().`is`(200))
+                .andExpect(jsonPath("songs.length()").value(0))
+    }
+    @Test
+    fun `앨범 이름으로 검색 결과 확인`(){
+        mvc.perform(
+                get("/api/v1/albums").param("keyword","end")
+        )
+                .andExpect(status().`is`(200))
+                .andExpect(jsonPath("albums.[0].title").value("Old Friend"))
+        mvc.perform(
+                get("/api/v1/albums").param("keyword","????????")
+        )
+                .andExpect(status().`is`(200))
+                .andExpect(jsonPath("albums.length()").value(0))
     }
 }
