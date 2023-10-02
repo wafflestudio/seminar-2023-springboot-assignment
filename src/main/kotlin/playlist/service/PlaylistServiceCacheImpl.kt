d4fb86a43de636ae38eb07187888e7c5cb510241
YJ0513 <myday05@snu.ac.kr>
2023-10-03 02:44:07 +0900

    커밋

--- src/main/kotlin/playlist/service/PlaylistServiceCacheImpl.kt
+++ src/main/kotlin/playlist/service/PlaylistServiceCacheImpl.kt
@@ -1,18 +1,41 @@
 package com.wafflestudio.seminar.spring2023.playlist.service
 
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.TimeUnit
 
 // TODO: 캐시 TTL이 10초가 되도록 캐시 구현체를 구현 (추가 과제)
 @Service
 class PlaylistServiceCacheImpl(
     private val impl: PlaylistServiceImpl,
 ) : PlaylistService {
-
+    private val playlistGroupCache: MutableMap<Long, PlaylistGroupCache> = ConcurrentHashMap()
+    private val playlistCache: MutableMap<Long, PlaylistCache> = ConcurrentHashMap()
     override fun getGroups(): List<PlaylistGroup> {
-        TODO("Not yet implemented")
+        val currentTime = System.currentTimeMillis()
+        val cachePlaylistGroup = playlistGroupCache[0]
+
+        if (cachePlaylistGroup != null && (currentTime - cachePlaylistGroup.time <= TimeUnit.SECONDS.toMillis(10))) {
+            return cachePlaylistGroup.playlistGroup
+        }
+
+        val group = impl.getGroups()
+        playlistGroupCache[0] = PlaylistGroupCache(group, currentTime)
+        return group
     }
 
     override fun get(id: Long): Playlist {
-        TODO("Not yet implemented")
+        val currentTime = System.currentTimeMillis()
+        val cachedPlaylist = playlistCache[id]
+
+        if (cachedPlaylist != null && (currentTime - cachedPlaylist.time <= TimeUnit.SECONDS.toMillis(10))) {
+            return cachedPlaylist.playlist
+        }
+
+        val playlist = impl.get(id)
+        playlistCache[id] = PlaylistCache(playlist, currentTime)
+        return playlist
     }
+    data class PlaylistCache(val playlist: Playlist, val time: Long)
+    data class PlaylistGroupCache(val playlistGroup: List<PlaylistGroup>, val time: Long)
 }
--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -1,13 +1,41 @@
 package com.wafflestudio.seminar.spring2023.song
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.transaction.annotation.Transactional
 
 @AutoConfigureMockMvc
+@Transactional
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
+    private val mapper: ObjectMapper,
 ) {
+    @Test
+    fun `노래 찾기`() {
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/songs")
+                .param("keyword", "Don't")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+    }
+    @Test
+    fun `앨범 찾기`() {
+        mvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/albums")
+                .param("keyword", "One")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+    }
 }
