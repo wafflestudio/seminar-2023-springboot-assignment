d538aabfef1968e3b4f82215f8a8dbc797388ed9
suhyuk <isuh88@snu.ac.kr>
2023-10-02 12:29:36 +0900

    fix: songIntegrationTest fixed

--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -8,9 +8,11 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.transaction.annotation.Transactional
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
 ) {
