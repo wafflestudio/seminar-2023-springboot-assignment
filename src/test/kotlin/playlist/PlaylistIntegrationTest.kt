8e62010bbed3c943e097510af5216992850e5a8b
eastshine2741 <eastshine@snu.ac.kr>
2023-10-02 14:32:06 +0900

    Controller 작성 및 IntegrationTest 통과

--- src/test/kotlin/playlist/PlaylistIntegrationTest.kt
+++ src/test/kotlin/playlist/PlaylistIntegrationTest.kt
@@ -1,13 +1,110 @@
 package com.wafflestudio.seminar.spring2023.playlist
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class PlaylistIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
+    private val mapper: ObjectMapper
 ) {
+    @BeforeEach
+    fun 회원가입() {
+        mvc.perform(
+            post("/api/v1/signup")
+                .content(
+                    mapper.writeValueAsString(
+                        mapOf(
+                            "username" to "test",
+                            "password" to "qwer1234",
+                            "image" to "https://wafflestudio.com/images/icon_intro.svg"
+                        )
+                    )
+                )
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().`is`(200))
+    }
+
+    @Test
+    fun `플레이리스트 그룹 조회`() {
+        mvc.perform(
+            get("/api/v1/playlist-groups")
+        ).andExpect(status().`is`(200))
+    }
+
+    @Test
+    fun `플레이리스트 조회`() {
+        mvc.perform(
+            get("/api/v1/playlists/1")
+        ).andExpect(status().`is`(200))
+    }
+
+    @Test
+    fun `존재하지 않는 플레이리스트를 조회한 경우 404`() {
+        mvc.perform(
+            get("/api/v1/playlists/404404404")
+        ).andExpect(status().`is`(404))
+    }
+
+    @Test
+    fun `로그아웃 상태에서 플레이리스트 좋아요 되어있지 않아야 함`() {
+        mvc.perform(
+                get("/api/v1/playlists/1")
+        ).andExpect(jsonPath("$.isLiked").value(false))
+    }
+
+    @Test
+    fun `로그아웃 상태에서 플레이리스트 좋아요 등록 or 해제 시 401`() {
+        mvc.perform(
+            post("/api/v1/playlists/1/likes")
+        ).andExpect(status().`is`(401))
+    }
+
+    @Test
+    fun `로그인 상태에서 플레이리스트 좋아요 등록 or 해제`() {
+        mvc.perform(
+            post("/api/v1/playlists/1/likes")
+                .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(200))
+
+        mvc.perform(
+            delete("/api/v1/playlists/1/likes")
+                .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(200))
+    }
+
+    @Test
+    fun `로그인 상태에서 플레이리스트 중복 좋아요 등록 or 해제 시 409`() {
+        mvc.perform(
+                post("/api/v1/playlists/1/likes")
+                        .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(200))
+
+        mvc.perform(
+                post("/api/v1/playlists/1/likes")
+                        .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(409))
+
+        mvc.perform(
+                delete("/api/v1/playlists/1/likes")
+                        .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(200))
+
+        mvc.perform(
+                delete("/api/v1/playlists/1/likes")
+                        .header("Authorization", "Bearer tset")
+        ).andExpect(status().`is`(409))
+    }
 }
--- src/test/kotlin/song/SongIntegrationTest.kt
+++ src/test/kotlin/song/SongIntegrationTest.kt
@@ -1,13 +1,41 @@
 package com.wafflestudio.seminar.spring2023.song
 
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
 class SongIntegrationTest @Autowired constructor(
     private val mvc: MockMvc,
 ) {
+    @Test
+    fun `제목에 키워드를 포함하는 곡 검색`() {
+        mvc.perform(
+            get("/api/v1/songs")
+                .param("keyword", "Shape")
+        )
+            .andExpect(status().`is`(200))
+            .andExpect(jsonPath("$.songs.length()").value(2))
+            .andExpect(jsonPath("$.songs[0].title").value("Shapes"))
+            .andExpect(jsonPath("$.songs[1].title").value("Shape of You"))
+    }
+
+    @Test
+    fun `제목에 키워드를 포함하는 앨범 검색`() {
+        mvc.perform(
+                get("/api/v1/albums")
+                    .param("keyword", "GUT")
+        )
+            .andExpect(status().`is`(200))
+            .andExpect(jsonPath("$.albums.length()").value(1))
+            .andExpect(jsonPath("$.albums[0].title").value("GUTS"))
+    }
 }
