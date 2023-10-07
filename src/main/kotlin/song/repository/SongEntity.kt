1b0ebd53d3538f88b965181c3a0d434120b6e135
Minkyu Lee <whizkyu@snu.ac.kr>
2023-10-07 15:26:50 +0900

    Add SongEntity

---
+++ src/main/kotlin/song/repository/SongEntity.kt
@@ -0,0 +1,23 @@
+package com.wafflestudio.seminar.spring2023.song.repository
+
+import jakarta.annotation.Nonnull
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity(name = "songs")
+class SongEntity (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+    val title: String,
+    @Nonnull
+    val duration: Int = 0,
+    @ManyToOne
+    @JoinColumn(name = "album_id")
+    val album: AlbumEntity,
+)
