3c82346e2c6d2e17616c0d80c527cd09fb08e97a
onemeee <dlgkssk38@snu.ac.kr>
2023-10-22 13:08:26 +0900

    week1 테스트 해결

--- src/main/kotlin/playlist/repository/PlaylistRepository.kt
+++ src/main/kotlin/playlist/repository/PlaylistRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface PlaylistRepository : JpaRepository<PlaylistEntity, Long> {
-    @Query("SELECT p FROM playlists p LEFT JOIN FETCH p.playlistSongs ps LEFT JOIN FETCH ps.song s WHERE p.id = :id ")
+    @Query("SELECT p FROM playlists p LEFT JOIN FETCH p.playlistSongs ps LEFT JOIN FETCH ps.song s LEFT JOIN FETCH s.album a LEFT JOIN FETCH a.artist WHERE p.id = :id ")
     fun findByIdWithJoinFetch(id: Long) : PlaylistEntity?
 }
